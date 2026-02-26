a35c31f849b20caca71539d2a8385022bf8be415
Jeff Adair <jeffadair@senzing.com>
2026-02-26 09:36:05 -0600

    trailing whitespace and avoiding crash on EOL when processing stdin

--- Casks/senzing-sdk.rb
+++ Casks/senzing-sdk.rb
@@ -3,7 +3,7 @@ cask "senzing-sdk" do
   sha256 "bdc842aded49fb602b626656a0d8102a2e127916bd4c0afd7102079c8183374b"
 
   url "https://senzing-staging-osx.s3.amazonaws.com/senzingsdk_#{version}.pkg"
-       
+
   name "Senzing SDK"
   desc "Senzing® Smarter Entity Resolution™ SDK with Entity Centric Learning™ technology. Connect Data. Power Intelligence.™ entity resolution."
   homepage "https://senzing.com"
@@ -27,7 +27,7 @@ cask "senzing-sdk" do
 
       EOS
       print "Do you accept the license terms? [yes/no]: "
-      response = $stdin.gets.chomp.downcase
+      response = $stdin.gets&.chomp&.downcase.to_s     
       unless %w[y yes 1 true].include?(response)
         raise Cask::CaskError, "License not accepted. Installation aborted."
       end
